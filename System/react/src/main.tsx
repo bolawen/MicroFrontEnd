01614c99dcc3b466785f7e47383e95a4f5a79446
szhangwenqiang_v@didichuxing.com <szhangwenqiang_v@didiglobal.com>
2022-08-12 11:33:07 +0800

    推送

--- System/react/src/main.tsx
+++ System/react/src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
-);
+const container = document.getElementById("root");
+ReactDOM.render(<App />, container);
